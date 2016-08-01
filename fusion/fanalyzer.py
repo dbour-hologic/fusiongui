372ba13f9bcf012cadf213c397b2423d93e55045
Bour <dbo1014@hologic.corp>
2016-08-01 14:43:40 -0700

    Finished Paraflu Specific PCR file modification

--- fusion/fanalyzer.py
+++ fusion/fanalyzer.py
@@ -28,9 +28,9 @@ class FusionAnalysis():
 
 		if (assay_type == 'Paraflu'):
 
-			self.pcr_file = pd.read_csv(lis_path,
+			self.pcr_file = pd.read_csv(pcr_path,
 										delimiter=',',
-										encoding='utf-8-sig'
+										encoding='utf-8-sig',
 										dtype={'CapAndVialTrayID': object,
 											   'ElutionBufferRFID': object,
 											   'OilRFID': object,
@@ -39,7 +39,7 @@ class FusionAnalysis():
 											   }
 									 	)
 
-			self.lis_file = pd.read_csv(pcr_path,
+			self.lis_file = pd.read_csv(lis_path,
 										delimiter='\t',
 										encoding='utf-8-sig',
 										dtype={'Test order #': object}
@@ -62,38 +62,49 @@ class FusionAnalysis():
 		CHANGE_PCR_COLUMN_DICT = {'RFU Range':'Unrounded RFU Range','LR_Ct_NonNormalized':'Unrounded Ct'}
 		CHANGE_LIS_COLUMN_NAMES = False
 		CHANGE_LIS_COLUMN_DICT = {}
+		PCR_COLUMNS_KEEP = ['Specimen Barcode', 'Analyte', 'Run ID', 
+                            'Channel', 'Unrounded RFU Range', 'EstimatedBaseline',
+                        	'Unrounded Ct', 'LR_TSlope_NonNormalized','Cartridge Lot #',
+                         	'CapAndVialTrayID','Test order #', 'FCRBarcode', 'FERBarcode',
+                         	'ElutionBufferRFID','ReconstitutionBufferRFID', 'OilRFID',
+                         	'WellID','FusionTestOrder']
 
 		if CHANGE_PCR_COLUMN_NAMES:
 			self.pcr_file.rename(columns=CHANGE_PCR_COLUMN_DICT, inplace=True)
 		if CHANGE_LIS_COLUMN_NAMES:
 			self.lis_file.rename(columnns=CHANGE_LIS_COLUMN_DICT, inplace=True)
 
+		# --- STARTING HERE IS PARAFLU SPECIFIC
+
 		# Partition the barcode numbers
+		self.pcr_file['CapAndVialTrayID'] = self.pcr_file['CapAndVialTrayID'].apply(lambda num: self.trimmer(num, trim_front=2, trim_back=10)) 
+		self.pcr_file['FCRBarcode'] = self.pcr_file['FCRBarcode'].apply(lambda num: self.trimmer(num, trim_front=4, trim_back=11))
+		self.pcr_file['FERBarcode'] = self.pcr_file['FERBarcode'].apply(lambda num: self.trimmer(num, trim_front=4, trim_back=11))
+		self.pcr_file['ElutionBufferRFID'] = self.pcr_file['ElutionBufferRFID'].apply(lambda num: self.trimmer(num, trim_front=4, trim_back=11))
+		self.pcr_file['ReconstitutionBufferRFID'] = self.pcr_file['ReconstitutionBufferRFID'].apply(lambda num: self.trimmer(num,trim_front=4, trim_back=11))
+		self.pcr_file['OilRFID'] = self.pcr_file['OilRFID'].apply(lambda num: self.trimmer(num, trim_front=4, trim_back=11))
 
-		self.pcr_file['CapAndVialTrayID'] = self.pcr_file['CapAndVialTrayID'].apply(lambda num: self.trimmer(num, 
-		                                                                                      trim_front=2, 
-		                                                                                      trim_back=10))
-		self.pcr_file['FCRBarcode'] = self.pcr_file['FCRBarcode'].apply(lambda num: trimmer(num, 
-		                                                                          trim_front=4, 
-		                                                                          trim_back=11))
-		self.pcr_file['FERBarcode'] = self.pcr_file['FERBarcode'].apply(lambda num: trimmer(num, 
-		                                                                          trim_front=4, 
-		                                                                          trim_back=11))
-		self.pcr_file['ElutionBufferRFID'] = self.pcr_file['ElutionBufferRFID'].apply(lambda num: trimmer(num, 
-		                                                                                        trim_front=4, 
-		                                                                                        trim_back=11))
-		self.pcr_file['ReconstitutionBufferRFID'] = self.pcr_file['ReconstitutionBufferRFID'].apply(lambda num: trimmer(num, 
-		                                                                                                      trim_front=4, 
-		                                                                                                      trim_back=11))
-		self.pcr_file['OilRFID'] = self.pcr_file['OilRFID'].apply(lambda num: trimmer(num, 
-		                                                                    trim_front=4, 
-		                                                                    trim_back=11))
+		pcr_file_filtered_columns = self.pcr_file[PCR_COLUMNS_KEEP]
 
-		print(pcr_file[['CapAndVialTrayID', 'FCRBarcode', 'FERBarcode', 
-          'ElutionBufferRFID', 'ReconstitutionBufferRFID',
-          'OilRFID']][:2])
+		# Add an extra column to help group the subjects. Will later to be used for combining with another dataframe.
+		pcr_file_filtered_columns = pcr_file_filtered_columns.assign(UniqueID = pcr_file_filtered_columns['Specimen Barcode'] + "_" + 
+                                                             					pcr_file_filtered_columns['Run ID'] + "_" + 
+                                                             				    pcr_file_filtered_columns['Test order #'])
 
-	def trimmer(number, trim_front=0, trim_back=0):
+		# Remove the '[end]' from 'Specimen Barcode', a designation for end of file that came from the automated Panther Software
+		pcr_file_filtered_columns = pcr_file_filtered_columns[~pcr_file_filtered_columns['Specimen Barcode'].str.contains('[end]')]
+
+		# Pivot the PCR File - the long to wide conversion
+		pivot_pcr_file = pcr_file_filtered_columns.pivot(index='UniqueID', columns='Channel')
+
+		# Merge columns from multilayer to single layer headers
+		pivot_pcr_file.columns = [name[1]+"-"+name[0] for name in pivot_pcr_file.columns.values]
+
+		# --- END PARAFLU SPECIFIC
+
+		print(pivot_pcr_file[:2])
+
+	def trimmer(self, number, trim_front=0, trim_back=0):
 
 		"""
 		Trimmer takes in an ID number, (obj) representation
@@ -125,4 +136,5 @@ class FusionAnalysis():
 		return object_to_string
 
 if __name__ == '__main__':
-	pass
+	f = FusionAnalysis("@Pt22090000574-20160701-183343-000574-20160701-04.lis", "@DI2090000574-20160701-215050-000574-20160701-04.csv", "Paraflu")
+	f.combine_files('Paraflu', 'here')
