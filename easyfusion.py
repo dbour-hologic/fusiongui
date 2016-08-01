4e2ab9252fe39af3a0c2af24811a3a4ad7ebbfb6
Bour <dbo1014@hologic.corp>
2016-08-01 11:11:19 -0700

    Finished with layout, adding functions

--- easyfusion.py
+++ easyfusion.py
@@ -9,12 +9,68 @@ class Fusion(QtGui.QWidget):
 
 		self.initUI()
 
+		self.pcr_files = []
+		self.lis_files = []
+
 	def initUI(self):
 
-		self.setGeometry(300,300,500,300)
-		self.setWindowTitle('Fusion LIS & PCR Analysis')
+		grid = QtGui.QGridLayout()
+
+		lisUploadBox = QtGui.QGroupBox("LIS Uploads", self)
+		pcrUploadBox = QtGui.QGroupBox("PCR Uploads", self)
+		assayTypeBox = QtGui.QGroupBox("Assay Type", self)
+		executeBox = QtGui.QGroupBox(self)
+
+		upload_lis_button = QtGui.QPushButton("Upload LIS File")
+		upload_pcr_button = QtGui.QPushButton("Upload PCR File")
+
+
+		lisFileList = QtGui.QListWidget()
+		pcrFileList = QtGui.QListWidget()
+
+		vboxLISFiles = QtGui.QVBoxLayout()
+		vboxLISFiles.addWidget(upload_lis_button)
+		vboxLISFiles.addWidget(lisFileList)
+		vboxLISFiles.addStretch(1)
+
+		vboxPCRFiles = QtGui.QVBoxLayout()
+		vboxPCRFiles.addWidget(upload_pcr_button)
+		vboxPCRFiles.addWidget(pcrFileList)
+		vboxPCRFiles.addStretch(1)
+
+		assay_type_paraflu = QtGui.QRadioButton("Paraflu")
+		assay_type_flu = QtGui.QRadioButton("Flu")
+
+		vboxAssay = QtGui.QVBoxLayout()
+		vboxAssay.addWidget(assay_type_paraflu)
+		vboxAssay.addWidget(assay_type_flu)
+		vboxAssay.addStretch(1)
+
+		execute_run = QtGui.QPushButton("Run")
+
+		vboxRun = QtGui.QVBoxLayout()
+		vboxRun.addWidget(execute_run)
+
+		lisUploadBox.setLayout(vboxLISFiles)
+		pcrUploadBox.setLayout(vboxPCRFiles)
+		assayTypeBox.setLayout(vboxAssay)
+		executeBox.setLayout(vboxRun)
+		
+		grid.addWidget(lisUploadBox, 1, 1)
+		grid.addWidget(pcrUploadBox, 1, 2)
+		grid.addWidget(assayTypeBox, 1, 3)
+		grid.addWidget(executeBox, 2, 1, 1, 3)
+		self.setLayout(grid)
+
+		self.setGeometry(300,300,500,250)
+		self.setWindowTitle('Fusion LIS & PCR Combiner')
 		self.show()
 
+	def populateFields(self):
+		"""
+		Populates a list widget with the files selected
+		"""
+		pass
 
 def main():
 
